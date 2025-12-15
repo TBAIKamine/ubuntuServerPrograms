d94c96e66c1c45da0018c73bb967ca4082a3a502
Amine Tbaik (Macbook pro) <tbaik.amine@gmail.com>
2025-12-15 17:25:12 +0100

    minor fixes -added missing tools: fail2ban with vpn bypass -fixed docker mailserver postfix missing dependencies

--- helpers/dms_install.sh
+++ helpers/dms_install.sh
@@ -8,6 +8,9 @@ chown user:user $DMS_DIR $DMS_DIR/compose.yaml $DMS_DIR/mailserver.env
 unset DMS_GITHUB_URL
 
 # compose.yaml edits with sed.
+if [[ -n "${FQDN:-}" ]]; then
+      sed -i -E "s/^([[:space:]]*hostname:[[:space:]]*)mail\.example\.com([[:space:]]*)$/\1mail.${FQDN}\2/" "$DMS_DIR/compose.yaml"
+fi
 sed -i '/env_file: mailserver.env/a\
     environment:\
       - SPOOF_PROTECTION=1\
@@ -21,7 +24,6 @@ sed -i '/env_file: mailserver.env/a\
       - ENABLE_AMAVIS=0\
       - ENABLE_OPENDKIM=0\
       - ENABLE_OPENDMARC=0\
-      - ENABLE_FAIL2BAN=1\
       - SSL_TYPE=letsencrypt\
       - POSTFIX_MESSAGE_SIZE_LIMIT=0\
       - PFLOGSUMM_TRIGGER=logrotate\
@@ -30,16 +32,20 @@ sed -i '/env_file: mailserver.env/a\
       - LOGWATCH_RECIPIENT=$DMS_EMAIL\
       - REPORT_SENDER=$DMS_EMAIL\
       - ENABLE_MTA_STS=1' $DMS_DIR/compose.yaml
-sed -i 's/^[[:space:]]*# cap_add:/    cap_add:/' $DMS_DIR/compose.yaml
-sed -i 's/^[[:space:]]*#   - NET_ADMIN/      - NET_ADMIN/' $DMS_DIR/compose.yaml
 sed -i '/^[[:space:]]*healthcheck:/,/^[[:space:]]*retries:/d' $DMS_DIR/compose.yaml
 awk -i inplace '{if ($0 ~ /^[[:space:]]*- "[0-9]+:[0-9]+"/) {match($0, /^([[:space:]]*- ")([0-9]+):([0-9]+)"/, arr); print arr[1] arr[2]+1000 ":" arr[3] "\""} else print}' $DMS_DIR/compose.yaml
+sed -i '/^  mailserver:/,/^  [A-Za-z0-9_-]\+:/ { /^[[:space:]]*volumes:[[:space:]]*$/a\
+      - /etc/letsencrypt/live:/etc/letsencrypt/live:ro\
+      - /etc/letsencrypt/archive:/etc/letsencrypt/archive:ro
+}' "$DMS_DIR/compose.yaml"
 
-mkdir -p $DMS_DIR/docker-data/dms/config
+mkdir -p $DMS_DIR/docker-data/dms/{mail-data,mail-state,mail-logs,config}
+chown -R dms:dms $DMS_DIR
+chown -R dms:dms $DMS_DIR/docker-data
 ABS_PATH=$(dirname "$(realpath "$0")")
 # custom config overrides
 cp $ABS_PATH/postfix-main.cf $DMS_DIR/docker-data/dms/config/postfix-main.cf
 cp $ABS_PATH/user-patches.sh $DMS_DIR/docker-data/dms/config/user-patches.sh
 
-chown -R user:user $DMS_DIR/docker-data/dms/config/{postfix-main.cf,user-patches.sh}
+chown -R dms:dms $DMS_DIR/docker-data/dms/config/{postfix-main.cf,user-patches.sh}
 chmod -R 555 $DMS_DIR/docker-data/dms/config/{postfix-main.cf,user-patches.sh}
