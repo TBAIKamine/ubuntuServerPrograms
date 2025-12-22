ab4ea5b87375bfa807b4e85d332ae2b3e85e6a76
Amine Tbaik (Macbook pro) <tbaik.amine@gmail.com>
2025-12-22 23:28:14 +0100

    fixed wrong fsubid broken copy path

--- helpers/podman_install.sh
+++ helpers/podman_install.sh
@@ -20,6 +20,3 @@ EOF
 chown -R "$SUDO_USER:$SUDO_USER" /home/$SUDO_USER/.config/containers/containers.conf
 chmod 644 /home/$SUDO_USER/.config/containers/containers.conf
 sudo -u $SUDO_USER podman system migrate
-ABS_PATH=$(dirname "$(realpath "$0")")
-cp "$ABS_PATH/fsubid.sh" /usr/local/bin/fsubid
-chmod +x /usr/local/bin/fsubid
